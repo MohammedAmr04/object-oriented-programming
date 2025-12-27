c1cde4aba3dfdb3a48c697e55a0261c772359b17
MohammedAmr04 <mohammedamr200497@gmail.com>
2025-12-27 16:24:19 +0200

    add encapsulation example and explain it

--- object-oriented-programming/Program.cs
+++ object-oriented-programming/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var Employee = new Employee();
+            // before encapsulation 
+            //Employee.FirstName = "Mohammed";
+            Employee.SetName("Mohammed", "Amr");
+            // before encapsulation
+            //Employee.BirthDate = new DateOnly(1700, 5, 23);
+            Employee.SetBirthDate(new DateOnly(1960,5,5));
+            // task make encapsulation to salary and tax
+            Employee.BasicSalary = -5000;
+            Employee.TaxPercentage = 140;
+
+            Console.WriteLine($" Employee Name is {Employee.FirstName} {Employee.LastName} ");
+            Console.WriteLine($" Employee Birthdate is {Employee.BirthDate}");
+            Console.WriteLine($" Employee BasicSalary is {Employee.BasicSalary}");
+            Console.WriteLine($" Employee TaxPercentage is {Employee.TaxPercentage}");
+
         }
     }
 }
