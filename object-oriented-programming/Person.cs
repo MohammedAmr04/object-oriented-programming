e483b20629e653146c6206b2f14f31eb1a8d442a
MohammedAmr04 <mohammedamr200497@gmail.com>
2025-12-27 17:00:56 +0200

    add inheritance example

---
+++ object-oriented-programming/Person.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace object_oriented_programming
+{
+    internal class Person
+    {
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public void SetName(string firstName, string lastName)
+        {
+            if (String.IsNullOrWhiteSpace(lastName) || String.IsNullOrWhiteSpace(firstName))
+            {
+                throw new ArgumentNullException("first or last name is null");
+            }
+            FirstName = firstName;
+            LastName = lastName;
+        }
+        public DateOnly BirthDate { get; private set; }
+
+        public void SetBirthDate(DateOnly date)
+        {
+            if (date < new DateOnly(1960, 1, 1))
+            {
+                throw new ArgumentException("Invalid BirthDate Value");
+
+            }
+            BirthDate = date;
+
+        }
+    }
+}
