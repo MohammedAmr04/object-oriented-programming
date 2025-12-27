53b0e659ec29c5333e1a499c11edd8003846d9e9
MohammedAmr04 <mohammedamr200497@gmail.com>
2025-12-27 17:55:20 +0200

    refactor Person class to be abstract and enhance encapsulation in Program

--- object-oriented-programming/Person.cs
+++ object-oriented-programming/Person.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace object_oriented_programming
 {
-    internal class Person
+//     We use abstract classes to represent base concepts
+// that should not be instantiated directly,
+// and to enforce a common contract for derived classes.
+
+// Inheritance is used only when there is a true "is-a" relationship.
+// The derived class must represent a specialized type of the base class,
+// not just share some common data or behavior.
+// If the relationship is "has-a", composition should be used instead.
+
+    internal abstract class Person
+    // Abstract class representing a generic person
+    // which cannot be instantiated directly
+    // why abstract?
+    // Because Person is a general concept
+    // and we want to enforce that only specific types of persons
+    
     {
         public string FirstName { get; private set; }
         public string LastName { get; private set; }
--- object-oriented-programming/Program.cs
+++ object-oriented-programming/Program.cs
@@ -5,13 +5,9 @@
         static void Main(string[] args)
         {
             var Employee = new Employee();
-            // before encapsulation 
-            //Employee.FirstName = "Mohammed";
+
             Employee.SetName("Mohammed", "Amr");
-            // before encapsulation
-            //Employee.BirthDate = new DateOnly(1700, 5, 23);
             Employee.SetBirthDate(new DateOnly(1960,5,5));
-            // task make encapsulation to salary and tax
             Employee.BasicSalary = -5000;
             Employee.TaxPercentage = 140;
 
@@ -24,11 +20,11 @@
             applicant.SetBirthDate(new DateOnly(2004,3,11));
             PersonDetails(applicant);
 
-
+            
             void PersonDetails(Person person)
             {
-                Console.WriteLine($" Employee Name is {person.FirstName} {person.LastName} ");
-                Console.WriteLine($" Employee Birthdate is {person.BirthDate}");
+                Console.WriteLine($"  Name is {person.FirstName} {person.LastName} ");
+                Console.WriteLine($"  Birthdate is {person.BirthDate}");
             }
         }
       
